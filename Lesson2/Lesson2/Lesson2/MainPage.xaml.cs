eebcbfc478b5f97701f04fc4ad1ce7d97ed51d0c
Y.S.LIM <gninraw@lonk.co.kr>
2017-11-16 09:56:05 +0900

    프로젝트 추가

---
+++ Lesson2/Lesson2/Lesson2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lesson2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
---
+++ Lesson2/Lesson2/Lesson2/ViewModel/PageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public class PageViewModel
+    {
+        private String _lab1;
+        public String Lab1
+        {
+            get
+            {
+                return _lab1;
+            }
+
+            set
+            {
+                _lab1 = value;
+            }
+        }
+
+        public String Lab1Text { get; set; }
+        public String Lab2Text { get; set; }
+        public String Lab3Text { get; set; }
+
+        public PageViewModel()
+        {
+            Lab1Text = "label!!!!!";
+            Lab2Text = "2 text";
+            Lab3Text = "3 text!!!!!!!";
+        }
+    }
+}
